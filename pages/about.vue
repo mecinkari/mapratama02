44a49e76668dfde7d81fad5eadb17c72003334c0
rubahkurus <77764211+rubahkurus@users.noreply.github.com>
2021-11-11 15:07:27 +0700

    .

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="py-5 max-w-2xl px-6 mx-auto">
     <div class="">
-      <h1 class="text-4xl font-bold">About</h1>
+      <h1 class="text-4xl font-bold">Hello!</h1>
     </div>
     <hr class="border my-4" />
     <div class="prose prose-lg prose-blue mx-auto">
+      <img
+        src="~/assets/profile.jpg"
+        class="w-40 rounded-full shadow"
+        alt="profile"
+      />
       <p>
         Nama saya "Muhammad Anugrah Pratama", tapi orang biasa panggil saya
         "Tama" atau "Anugrah". Saya kelahiran Jakarta, 18 Desember 2002. Saat
@@ -22,7 +27,7 @@
       </p>
       <hr />
       <p>
-        Website ini dibuat menggunakan
+        Website ini dibuat oleh saya dari awal menggunakan
         <a href="http://vuejs.org" target="_blank">Vue.js</a>,
         <a href="http://nuxtjs.org" target="_blank">NuxtJS</a>
       </p>
@@ -31,5 +36,21 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    let typo = document.createElement("link");
+    typo.setAttribute("rel", "stylesheet");
+    typo.setAttribute(
+      "href",
+      "https://cdn.jsdelivr.net/npm/@tailwindcss/typography@0.4.x/dist/typography.min.css"
+    );
+    document.head.appendChild(typo);
+  },
+};
 </script>
+
+<style scoped>
+img {
+  margin: 0 auto;
+}
+</style>
